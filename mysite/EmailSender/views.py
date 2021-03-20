11ced72577e0c5bf7d536380915058229e9da70b
SmileyBoy321 <ungurkristen@gmail.com>
2021-03-20 20:47:09 +0200

    header and send_email button added

--- mysite/EmailSender/views.py
+++ mysite/EmailSender/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django import template
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Home page")
+        return render(request, "EmailSender/index.html")
