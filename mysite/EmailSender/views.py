1b368456b567d7b7e364a9e144b6fcb2f2ab2845
SmileyBoy321 <ungurkristen@gmail.com>
2021-03-23 22:07:47 +0200

    Removed JS and slapped Bootstrap into my project.

--- mysite/EmailSender/views.py
+++ mysite/EmailSender/views.py
@@ -1,6 +1,10 @@
 from django import template
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 
 def index(request):
         return render(request, "EmailSender/index.html")
+
+def compose(request):
+        return render(request, "EmailSender/email_template.html")
