8defa0dcaf46df4cb37b625e0df420c7edc353b4
SmileyBoy321 <ungurkristen@gmail.com>
2021-04-05 22:50:50 +0300

    start over fresh

--- mysite/EmailSender/urls.py
+++ mysite/EmailSender/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("compose/", views.compose, name="compose"),
+    path("", views.index),
 ]
--- mysite/EmailSender/views.py
+++ mysite/EmailSender/views.py
@@ -1,10 +1,5 @@
-from django import template
 from django.http.response import HttpResponse
-from django.shortcuts import render
 
 
 def index(request):
-        return render(request, "EmailSender/index.html")
-
-def compose(request):
-        return render(request, "EmailSender/email_template.html")
+        return HttpResponse("Hello")
