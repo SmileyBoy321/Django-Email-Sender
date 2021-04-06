35c977501563021bde3808e0841f839ce06220a3
SmileyBoy321 <ungurkristen@gmail.com>
2021-04-06 16:10:07 +0300

    Test changes

--- mysite/EmailSender/views.py
+++ mysite/EmailSender/views.py
@@ -1,5 +1,15 @@
-from django.http.response import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect, render
+
+from .forms import NameForm
 
 
-def index(request):
-        return HttpResponse("Hello")
+def get_name(request):
+    if request.method == "POST":
+        form = NameForm(request.POST)
+        if form.is_valid():
+            return render(request, "thanks.html")
+    else:
+        form = NameForm()
+
+    return render(request, "email_template.html", {"form": form})
