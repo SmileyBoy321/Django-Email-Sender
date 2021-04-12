6c9fa5e050e0ccfd8f707e0f594874851c96fe9a
SmileyBoy321 <ungurkristen@gmail.com>
2021-04-12 23:17:20 +0300

    work on email template

--- mysite/EmailSender/forms.py
+++ mysite/EmailSender/forms.py
@@ -3,9 +3,17 @@ from django.forms.widgets import EmailInput, TextInput
 
 
 class ComposeForm(forms.Form):
-    email_to = forms.EmailField(label="To", widget=EmailInput)
-    email_cc = forms.EmailField(label="CC", widget=EmailInput)
-    email_subject = forms.CharField(
-        required=False, widget=TextInput(attrs={"placeholder": "Subject"})
+    # Multiple email addresses to email_cc. Check the last answer in the link below.
+    # https://stackoverflow.com/questions/12698474/django-form-validation-accepting-multiple-values-for-one-field
+    email_to = forms.EmailField(label="To", widget=EmailInput(attrs={"size": 76}))
+    email_cc = forms.EmailField(
+        label="CC",
+        required=False,
+        widget=EmailInput(attrs={"size": 76, "multiple": True}),
     )
-    email_message = forms.CharField(required=True, label="", widget=forms.Textarea())
+    email_subject = forms.CharField(
+        required=False, widget=TextInput(attrs={"placeholder": "Subject", "size": 76})
+    )
+    email_message = forms.CharField(
+        required=True, label="", widget=forms.Textarea(attrs={"rows": 19, "cols": 78})
+    )
--- mysite/EmailSender/views.py
+++ mysite/EmailSender/views.py
@@ -5,7 +5,18 @@ from .forms import ComposeForm
 
 
 def email_template(request):
-    form = ComposeForm()
-    if form.is_valid():
-        print(form.cleaned_data)
+    if request.method == "GET":
+        form = ComposeForm()
+    else:
+        form = ComposeForm(request.POST)
+        if form.is_valid():
+            print(form)
+            email_to = form.cleaned_data["email_to"]
+            email_cc = form.cleaned_data["email_cc"]
+            email_subject = form.cleaned_data["email_subject"]
+            email_message = form.cleaned_data["email_message"]
+
+            print("DEBUG:", email_to)
+        else:
+            print("DEBUG:", form.errors)
     return render(request, "email_template.html", {"form": form})
