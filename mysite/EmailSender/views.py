0224d22e893ece51a213640e1cdd28e3e29a7ff9
SmileyBoy321 <ungurkristen@gmail.com>
2021-04-12 08:10:38 +0300

    testing the clean_data view

--- mysite/EmailSender/views.py
+++ mysite/EmailSender/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import redirect, render
 
 from .forms import ComposeForm
 
+
 def email_template(request):
     form = ComposeForm()
+    if form.is_valid():
+        print(form.cleaned_data)
     return render(request, "email_template.html", {"form": form})
